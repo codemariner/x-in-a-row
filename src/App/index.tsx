e82c7a21a945fde90ce98d06a28375e5837aa2bd
Scott Sayles <scott.sayles@gmail.com>
2019-08-23 12:02:51 -0400

    Add initial project files.

---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './index.scss'
+
+const App:React.StatelessComponent<{}> = () => (
+  <div className='container'>
+    <h1>Hello World, React!</h1>
+  </div>
+)
+
+export default App

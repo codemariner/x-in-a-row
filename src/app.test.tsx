0b14439405bee6863367b6d9a5edc1d339479f82
Scott Sayles <scott.sayles@gmail.com>
2019-08-23 12:41:24 -0400

    Move files.
    Just because I don't like the src/App layout since App is effectively
    our 'root'.

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from './index'
+
+import App from './app'
 
 describe('App', () => {
   it('renders without crashing', () => {

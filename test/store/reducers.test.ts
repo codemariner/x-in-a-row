e654db19ddded0e9191d9c755691de5ed296a5f1
Scott Sayles <scott.sayles@gmail.com>
2019-08-27 18:01:59 -0400

    Add test for reducer state for horizontal winner

---
+++ test/store/reducers.test.ts
@@ -0,0 +1,34 @@
+import configureStore from '../../src/store'
+import { initializeBoard, selectCell } from '../../src/store/actions'
+import { Store } from 'redux';
+
+describe('reducers', () => {
+
+	let store:Store
+
+	beforeEach(() => {
+		store = configureStore()
+	})
+
+	describe('selectCell', () => {
+		beforeEach(() => {
+			store.dispatch(initializeBoard(5, 5, 3))
+		})
+
+		it('should assign a winner for horizontal', () => {
+			expect(store.getState().winner).toBe(undefined)
+
+			store.getState().nextValue = 'X'
+			store.dispatch(selectCell(0,0));
+
+			store.getState().nextValue = 'X'
+			store.dispatch(selectCell(1,0));
+
+			store.getState().nextValue = 'X'
+			store.dispatch(selectCell(2,0));
+
+			expect(store.getState().winner).toBe('X')
+		})
+	})
+
+})
